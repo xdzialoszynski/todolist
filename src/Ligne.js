1ff5925eb378191efa14fc3a8ec3b1b8280ce56d
xdzialoszynski <xdzialoszynski@gmail.com>
2020-02-25 16:58:40 +0100

    LigneChangedToFunctionComponent

--- src/Ligne.js
+++ src/Ligne.js
@@ -2,34 +2,29 @@ import React from "react";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
-class Ligne extends React.Component {
-  constructor(props) {
-    super(props);    
-  }
+function Ligne (props) {  
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     console.log(
       "inside handlechange:value:" +
         event.target.value +
         "index:" +
-        this.props.index
+        props.index
     );
-    this.props.onLineChange(event.target.value, this.props.index);
+    props.onLineChange(event.target.value, props.index);
   }
-
-
-  render() {
+  
     return (
       <div>        
-        <TextField onChange={this.props.onLineChange}>
-          {this.props.value}
+        <TextField onChange={props.onLineChange}>
+          {props.value}
         </TextField>
-        <Button color="primary" variant="outlined" size="small" onClick={this.props.onDelete}>
+        <Button color="primary" variant="outlined" size="small" onClick={props.onDelete}>
           X
         </Button>
       </div>
     );
-  }
+  
 }
 
 export default Ligne;
